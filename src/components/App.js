71c4fe36197e0a1872e39c42ea7b1dfd52375bd5
DhanushM12 <dhanushmukhi@gmail.com>
2020-09-17 20:58:38 +0530

    created the sign in/up component and added route to get the sign in/up

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { SignIn } from './';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Slack Clone</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/login" component={SignIn} />
+          <Route exact path="/signup" component={SignIn} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="signin-form">
+        <h1>Sign In / Sign Up</h1>
+        <button className="btn basic-btn">Sign In / Sign Up with Google</button>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
