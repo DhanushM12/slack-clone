c6bee26506eca6d4b546ca1c7edc1c7ea4d14568
DhanushM12 <dhanushmukhi@gmail.com>
2020-09-19 03:01:34 +0530

    added the sending and receiving messages and added the username which is signed in and also displayed time when each msg are sent

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,43 +1,41 @@
-import React, { Component, useEffect } from 'react';
-import { signOut, firestore } from '../firebase';
+import React, { useContext } from 'react';
+import { signOut, auth } from '../firebase';
 import { Link } from 'react-router-dom';
 
-class Sidebar extends Component {
-  render() {
-    const { channels } = this.props;
-    return (
-      <div id="sidebar">
-        <div className="user-profile">
-          <div className="avatar">
-            <img src="https://www.flaticon.com/svg/static/icons/svg/2919/2919600.svg" />
-          </div>
-          <div>Ronald</div>
-          <div
-            style={{ marginLeft: 10, marginTop: 2, cursor: 'pointer' }}
-            onClick={signOut}
-          >
-            <img
-              src="https://www.flaticon.com/svg/static/icons/svg/2150/2150480.svg"
-              height="25"
-            />
-          </div>
+function Sidebar(props) {
+  const { channels } = props;
+  return (
+    <div id="sidebar">
+      <div className="user-profile">
+        <div className="avatar">
+          <img src="https://www.flaticon.com/svg/static/icons/svg/2919/2919600.svg" />
         </div>
-        <hr className="sidebar-spacer" />
-        <div className="user-channels"></div>{' '}
-        <div className="channels">
-          <div className="header">Channels</div>
-
-          <ul className="channels-list">
-            {channels.map((channel) => (
-              <li key={channel.id}>
-                <Link to={`/?id=${channel.id}`}># {channel.name}</Link>
-              </li>
-            ))}
-          </ul>
+        <div>{auth.currentUser.displayName}</div>
+        <div
+          style={{ marginLeft: 10, marginTop: 2, cursor: 'pointer' }}
+          onClick={signOut}
+        >
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/2150/2150480.svg"
+            height="25"
+          />
         </div>
       </div>
-    );
-  }
+      <hr className="sidebar-spacer" />
+      <div className="user-channels"></div>{' '}
+      <div className="channels">
+        <div className="header">Channels</div>
+
+        <ul className="channels-list">
+          {channels.map((channel) => (
+            <li key={channel.id}>
+              <Link to={`/?id=${channel.id}`}># {channel.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
